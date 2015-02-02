c2245eaf3d5f30eb9ed475db9d5f707e520b5f63
dajevu <jdavis@zazarie.com>
2015-02-02 13:55:54 -0700

    Added README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo is part of the Docker tutorial at: https://www.linkedin.com/pulse/article/5968117539400671232
--- runDockerPythonTTY.sh
+++ runDockerPythonTTY.sh
@@ -1,4 +1,4 @@
 
-docker run -t -i --name python -p 8000:8000 --link tomcat-maven:tomcat-maven--rm jeffdavisco/polymer-python:latest
+docker run -t -i --name python -p 8000:8000 --link tomcat-maven:tomcat-maven --rm jeffdavisco/polymer-python:latest
 
 # once running you can identify the IP addr by using: boot2docker ip. Then, use that IP to access port 8080
