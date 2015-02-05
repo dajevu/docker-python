c3233e9e16c0c019719de81fa004726f6ceb7dba
dajevu <jdavis@zazarie.com>
2015-02-04 19:32:05 -0700

    fix

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@ echo `env`
 git pull
 
 # replace placeholder with actual docker host
+sed -i "s/VMHOST/$TOMCAT_MAVEN_PORT_8080_TCP_ADDR/g" post-service/post-service.html
 
 python -m SimpleHTTPServer 8000
